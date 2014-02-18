a59c8531c6460309062c2dcd3e5d149072622d47
Luis Alonso <luis.alonso.112@gmail.com>
2014-02-18 13:58:23 +0100

    other change

--- uem.basesDeDatos.alonsostein.git/src/activity10/Pbes.java
+++ uem.basesDeDatos.alonsostein.git/src/activity10/Pbes.java
@@ -272,7 +272,8 @@ public class Pbes extends PbesAbstract implements ActionListener {
 	public void onPayByUserId(ActionEvent ae) {
 		String userIdText = txtSearch.getText(); // get the text form
 		// textfield
-		// TESTING CHANGE
+		// TESTING CHANGE 
+		// other change
 		Integer userId = Integer.parseInt(userIdText);
 		Customer myCustomer = (Customer) this.getCustomer(userId);
 		GuiFilter paymentQuestion = new GuiFilter(2);
