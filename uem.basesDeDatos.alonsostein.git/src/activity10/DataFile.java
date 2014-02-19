8dad8cb42a685b8660b84cb972ee72f5e4e25985
Luis <Luis@Luis-PC>
2014-02-19 12:45:29 +0100

    Created DataFile class to work with customers. Still work to do. Writing files should work though

---
+++ uem.basesDeDatos.alonsostein.git/src/activity10/DataFile.java
@@ -0,0 +1,96 @@
+package activity10;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DataFile {
+	private Customer[] customersToWrite;
+
+	/*
+	 * public static void main(String[] args) { DataFile w = new DataFile();
+	 * w.write();
+	 * 
+	 * JFrame frame = new JFrame("R/W Files");
+	 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); JLabel label = new
+	 * JLabel(w.readFile()); frame.getContentPane().add(label); frame.pack();
+	 * frame.setVisible(true);
+	 * 
+	 * }
+	 */
+	public void exportText() {
+		Integer maxCustomers = 0;
+		try {
+			GuiFilter a = new GuiFilter(0);
+
+			while (maxCustomers == 0) {
+				maxCustomers = a.getNumber();
+				Thread.sleep(200); // waits a time in miliseconds
+				customersToWrite = new Customer[maxCustomers];
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// 1. OPEN
+		String fileName = "C:\\data.txt"; // sets the file name we
+											// want to use and its
+											// location
+		try {
+			// 2. WRITE
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,
+					true)); // Creates a buffered character-output stream of a
+							// class for writing character files
+			for (int i = 0; i <= maxCustomers; i++) {
+				writer.write(customersToWrite[i].name); // writes a string with the number
+				writer.write(customersToWrite[i].id); // writes a string id
+				writer.write(customersToWrite[i].cellPhoneNumber);
+				writer.write(customersToWrite[i].landlinePhoneNumber);
+				writer.write(customersToWrite[i].airtimeMinutes);
+				writer.write(customersToWrite[i].balance);
+				writer.write(customersToWrite[i].rate);
+			}
+			// 3. CLOSE
+			writer.close(); // close the file
+		} catch (IOException e) {
+			e.printStackTrace(); // throws a standard error
+		}
+	}
+
+	// TODO find out how to do the read file
+	public String importText() {
+		// 1. OPEN file
+		String fileName = "C:\\data.txt";
+		String LS = System.getProperty("line.separator");
+		StringBuffer fileContent = new StringBuffer();
+
+		try {
+			// 2. READ file
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				fileContent.append(line).append(LS); // concatenate strings
+			}
+			// 3. CLOSE file
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return fileContent.toString();
+
+	}
+
+	public DataFile(Customer[] customers) {
+
+	}
+
+}
