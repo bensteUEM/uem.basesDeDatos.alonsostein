c50f87cad1f64d36e928cdefeaf7de3013cf1f61
benste <Benedict.Stein+git@gmail.com>
2014-02-24 22:58:13 +0100

    added more comments to GuiCustomerList

--- uem.basesDeDatos.alonsostein.git/src/activity10/GuiCustomerList.java
+++ uem.basesDeDatos.alonsostein.git/src/activity10/GuiCustomerList.java
@@ -14,19 +14,21 @@ import javax.swing.JScrollPane;
 /**
  * Simple class to display a list of customers
  * 
- * @author steinb
+ * @author benste
  * 
  */
 public class GuiCustomerList extends JFrame implements ActionListener {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8158363265686707093L;
 	private JPanel contentPane;
 
 	/**
-	 * A simple window with a message and a scrollable List of Customers
+	 * Constructor based on an existing list and some additional explanatory
+	 * text
+	 * 
+	 * @param listContent
+	 *            Customer Objects to be displayed
+	 * @param information
+	 *            Text to be displayed
 	 */
 	@SuppressWarnings("unchecked")
 	public GuiCustomerList(ArrayList<Customer> listContent, String information) {
@@ -35,28 +37,22 @@ public class GuiCustomerList extends JFrame implements ActionListener {
 		contentPane.setLayout(new BorderLayout(0, 0));// Set a layout
 		setContentPane(contentPane); // set content pane
 
-		JLabel lblinformation = new JLabel(information); // create information
-															// label on top
-															// north orientation
-															// with predefined
-															// text
-		contentPane.add(lblinformation, BorderLayout.NORTH); // add the
-																// information
+		JLabel lblinformation = new JLabel(information); 
+		// create information label on top north orientation
+		// with predefined text
+		contentPane.add(lblinformation, BorderLayout.NORTH); 
+		// add the information
 
 		@SuppressWarnings("rawtypes")
 		// this will auto convert to the correct class
-		JList<Customer> list = new JList(listContent.toArray());// insert the
-																// predefined
-																// array into
-																// the list
-
-		JScrollPane scrollPane = new JScrollPane(list); // add the Jlist to a
-														// scrollable container
-		contentPane.add(scrollPane, BorderLayout.CENTER); // add scrollpane to
-															// Frame
-
+		JList<Customer> list = new JList(listContent.toArray());
+		// insert the predefined array into the list
+		JScrollPane scrollPane = new JScrollPane(list);
+		// add the Jlist to a scrollable container
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		// add scrollpane to Frame
 		contentPane.setVisible(true); // make it visble
-	}
+	} //End GuiCustomerList()
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
@@ -64,5 +60,5 @@ public class GuiCustomerList extends JFrame implements ActionListener {
 		// TODO might include future feature to click on customer and see its
 		// values
 
-	}
+	} // End of actionperformed
 }
