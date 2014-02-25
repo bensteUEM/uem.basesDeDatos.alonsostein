b71baab33742d33abce630fe56817f433d497d57
benste <Benedict.Stein+git@gmail.com>
2014-02-25 13:18:14 +0100

    removed warnings from DataFile with unused content

--- uem.basesDeDatos.alonsostein.git/src/activity10/DataFile.java
+++ uem.basesDeDatos.alonsostein.git/src/activity10/DataFile.java
@@ -7,11 +7,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class DataFile {
 	private Customer[] groupOfCustomers;
-	private Customer oneCustomerToWrite;
 	private int numberOfLines = 0;
 
 	/*
