f5cc34b6c9df67f58aad3b99ece837883ee6ec3c
benste <Benedict.Stein+git@gmail.com>
2014-02-27 17:58:35 +0100

    Implemented Issue #12 - CustomerCall Class

---
+++ uem.basesDeDatos.alonsostein.git/src/activity10/CustomerCall.java
@@ -0,0 +1,62 @@
+package activity10;
+
+public class CustomerCall {
+
+	private Customer origin;
+	private String destination;
+	private Integer duration;
+
+	private CustomerCall() {
+	}
+
+	/**
+	 * Constructor for a new CustomerCall Object, will be initialized with cost
+	 * and and duration 0 but does require two parameters
+	 * 
+	 * @param origin
+	 *            Customer object of the Caller who will pay for it
+	 * @param destination
+	 *            String identiying the destination of the Call
+	 * @author benste
+	 */
+	public CustomerCall(Customer newOrigin, String newDestination) {
+		// Save params
+		this.origin = newOrigin;
+		this.destination = newDestination;
+
+		// Initialize other values
+		this.duration = 0;
+	}
+
+	/**
+	 * @return the destination
+	 */
+	public String getDestination() {
+		return destination;
+	}
+
+	/**
+	 * @return the duration
+	 */
+	public Integer getDuration() {
+		return duration;
+	}
+
+	/**
+	 * @param duration
+	 *            the duration to set
+	 */
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 * This method does calculate the total costs in Cent of the Call
+	 * 
+	 * @return costs in Cents
+	 * @author benste
+	 */
+	public Integer getTotal() {
+		return duration * origin.getRate();
+	}
+}
