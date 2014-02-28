c225c1f23dfb4dfecccf8cf683b3e1e6058dfa46
benste <Benedict.Stein+git@gmail.com>
2014-02-28 11:50:02 +0100

    Finished experiments on Excel import export
    Order only taken into account with linked ahsmap

--- uem.basesDeDatos.alonsostein.git/src/activity12/ExcelBenste.java
+++ uem.basesDeDatos.alonsostein.git/src/activity12/ExcelBenste.java
@@ -1,11 +1,14 @@
 package activity12;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Date;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,52 +27,98 @@ public class ExcelBenste {
 		writeExcel2013(new File("benste.xlsx"));
 		readExcel2013(new File("benste.xlsx"));
 	}
-	
-	public static void writeExcel2013(File theFile){
+
+	public static void writeExcel2013(File theFile) {
+		System.out.println("writeExcelFunction");
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("Sample sheet");
-		 
-		Map<String, Object[]> data = new HashMap<String, Object[]>();
-		data.put("1", new Object[] {"Emp No.", "Name", "Salary"});
-		data.put("2", new Object[] {1d, "John", 1500000d});
-		data.put("3", new Object[] {2d, "Sam", 800000d});
-		data.put("4", new Object[] {3d, "Dean", 700000d});
-		 
+
+		Map<String, Object[]> data = new LinkedHashMap<String, Object[]>();
+		data.put("1", new Object[] { "Emp No.", "Name", "Salary" });
+		data.put("2", new Object[] { 1d, "John", 1500000d });
+		data.put("3", new Object[] { 2d, "Sam", 800000d });
+		data.put("4", new Object[] { 3d, "Dean", 700000d });
+		
+		System.out.println(data); //DEBUG
 		Set<String> keyset = data.keySet();
 		int rownum = 0;
+		System.out.println(keyset);//DEBUG
 		for (String key : keyset) {
-		    Row row = sheet.createRow(rownum++);
-		    Object [] objArr = data.get(key);
-		    int cellnum = 0;
-		    for (Object obj : objArr) {
-		        Cell cell = row.createCell(cellnum++);
-		        if(obj instanceof Date) 
-		            cell.setCellValue((Date)obj);
-		        else if(obj instanceof Boolean)
-		            cell.setCellValue((Boolean)obj);
-		        else if(obj instanceof String)
-		            cell.setCellValue((String)obj);
-		        else if(obj instanceof Double)
-		            cell.setCellValue((Double)obj);
-		    }
+			Row row = sheet.createRow(rownum++);
+			Object[] objArr = data.get(key);
+			int cellnum = 0;
+			for (Object obj : objArr) {
+				System.out.println();
+				Cell cell = row.createCell(cellnum++);
+				if (obj instanceof Date)
+					cell.setCellValue((Date) obj);
+				else if (obj instanceof Boolean)
+					cell.setCellValue((Boolean) obj);
+				else if (obj instanceof String)
+					cell.setCellValue((String) obj);
+				else if (obj instanceof Double)
+					cell.setCellValue((Double) obj);
+			}
 		}
-		 
+
 		try {
-		    FileOutputStream out = 
-		            new FileOutputStream(theFile);
-		    workbook.write(out);
-		    out.close();
-		    System.out.println("Excel written successfully..");
-		     
+			FileOutputStream out = new FileOutputStream(theFile);
+			workbook.write(out);
+			out.close();
+			System.out.println("Excel written successfully..");
+
 		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-	
-	public static void readExcel2013(File theFile){
-		
+
+	public static void readExcel2013(File theFile) {
+		System.out.println("readExcelFunction");
+		try {
+
+			FileInputStream file = new FileInputStream(theFile);
+
+			// Get the workbook instance for XLS file
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			// Get first sheet from the workbook
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			// Iterate through each rows from first sheet
+			Iterator<Row> rowIterator = sheet.iterator();
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+
+				// For each row, iterate through each columns
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+
+					Cell cell = cellIterator.next();
+
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_BOOLEAN:
+						System.out.print(cell.getBooleanCellValue() + "\t\t");
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t\t");
+						break;
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t\t");
+						break;
+					}
+				}
+				System.out.println("");
+			}
+			file.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
