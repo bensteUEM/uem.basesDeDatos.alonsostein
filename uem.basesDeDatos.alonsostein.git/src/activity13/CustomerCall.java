067150e68bb35c2326a8d956695072f75ba48926
Luis <Luis@Luis-PC>
2014-03-05 17:44:02 +0100

    Added comments to CustomerCall

--- uem.basesDeDatos.alonsostein.git/src/activity13/CustomerCall.java
+++ uem.basesDeDatos.alonsostein.git/src/activity13/CustomerCall.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import activity10.Customer;
 
 public class CustomerCall {
-
+	// declaration of the class variables needed
 	private Customer origin;
 	private String destination;
 	private Integer duration; // in seconds
@@ -30,36 +30,43 @@ public class CustomerCall {
 	 * @author benste
 	 */
 	public CustomerCall(Customer newOrigin, String newDestination,
-			Calendar startTime) {
+			Calendar startTime) { // constructor for Customer call
 		// Save params
-		this.origin = newOrigin;
-		this.destination = newDestination;
-		this.startTime = startTime;
+		this.origin = newOrigin; // set the customer who makes the call
+		this.destination = newDestination; // set the destination of the call
+		this.startTime = startTime; // set the start time
 
 		// Initialize other values
-		this.duration = 0;
+		this.duration = 0; // initialize the duration to 0
 	}// End Customer Call default constructor
 
 	/**
 	 * @return the destination
 	 */
-	public String getDestination() {
+	public String getDestination() { // getter fot destination
 		return destination;
 	} // end getDestination
 
 	/**
 	 * Calculate the Call Time in Seconds based on the End Time of the Call
 	 */
-	public Integer calculateDuration(Calendar endTime) {
+	public Integer calculateDuration(Calendar endTime) { // method th calculate
+															// the call duration
+															// passing the
+															// endTime
 		this.duration = (int) ((endTime.getTimeInMillis() - this.startTime
-				.getTimeInMillis()) / 1000);
-		return duration;
+				.getTimeInMillis()) / 1000); // difference between the end time
+												// and the start time would give
+												// the call time in millisecods.
+												// Divide it by 1000 to get it
+												// in seconds
+		return duration; // return the duration of the call
 	} // end calculateDuration
 
 	/**
 	 * @return the duration
 	 */
-	public Integer getDuration() {
+	public Integer getDuration() { // getter for the duration
 		return duration;
 	} // end getDuration
 
@@ -67,7 +74,7 @@ public class CustomerCall {
 	 * @param duration
 	 *            the duration to set
 	 */
-	public void setDuration(Integer duration) {
+	public void setDuration(Integer duration) { // setter for duration
 		this.duration = duration;
 	} // end setDuration
 
@@ -77,18 +84,31 @@ public class CustomerCall {
 	 * @return costs in Cents
 	 * @author benste
 	 */
-	public BigDecimal getTotal() {
-		BigDecimal total = new BigDecimal((duration * origin.getRate())/100.00, new MathContext(3
-			));
-		//System.out.println(total); //TODO DEBUG
+	public BigDecimal getTotal() { // method to calculate the total cost of the
+									// call
+		BigDecimal total = new BigDecimal(
+				(duration * origin.getRate()) / 100.00, new MathContext(3)); // calculates
+																				// the
+																				// total
+																				// with
+																				// 3
+																				// significative
+																				// digits
+																				// as
+																				// the
+																				// (duration*the
+																				// rate
+																				// of
+																				// the
+																				// customer)/100
+		// System.out.println(total); //TODO DEBUG
 		return total;
 	} // end getTotal
 
 	/**
-	 * @return startTime
-	 *            the time the call started
+	 * @return startTime the time the call started
 	 */
-	public Date getStartTime() {
+	public Date getStartTime() {	// getter for the start time of the call
 		return this.startTime.getTime();
-	}
-}
+	}	// end of getStartTime
+} // end of class CustomerCall
