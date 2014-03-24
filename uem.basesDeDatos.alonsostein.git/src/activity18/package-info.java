3a60d02f19c25abbc49b44884e501033ad9f4147
bensteUEM <Benedict.Stein+git@gmail.com>
2014-03-24 13:27:49 +0100

    added activity 18 with SQL lib

---
+++ uem.basesDeDatos.alonsostein.git/src/activity18/package-info.java
@@ -0,0 +1,25 @@
+/**
+
+Java Programming
+
+
+Activity
+
+
+JDBC
+
+In this activity, you will learn how to use JDBC to execute SQL commands from your Java program.
+
+
+Procedure
+
+Study the online lessons "JDBC Introduction"  located at http://docs.oracle.com/javase/tutorial/jdbc/index.html.
+Study, and execute the samples, in the SQLite Java Tutorial at http://www.tutorialspoint.com/sqlite/sqlite_java.htm . Make sure you include the sqlite-jdbc jar file as an external library in your java project. Instead of executing the tutorial programs from the command line, you can do it from Eclipse.
+
+
+
+Prof. Nacho Giráldez, PhD
+ignacio.giraldez@uem.es
+ *
+ */
+package activity18;
---
+++ uem.basesDeDatos.alonsostein.git/src/activity18/testBenste.java
@@ -0,0 +1,13 @@
+package activity18;
+
+public class testBenste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
