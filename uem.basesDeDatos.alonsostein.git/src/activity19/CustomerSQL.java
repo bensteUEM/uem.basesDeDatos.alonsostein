0161161e6703e0dd194b0aba13df54918069e2a8
bensteUEM <Benedict.Stein+git@gmail.com>
2014-03-31 13:08:09 +0200

    export sql values

--- uem.basesDeDatos.alonsostein.git/src/activity19/CustomerSQL.java
+++ uem.basesDeDatos.alonsostein.git/src/activity19/CustomerSQL.java
@@ -14,22 +14,51 @@ public class CustomerSQL extends Customer {
 
 	public CustomerSQL(String newName, String newCellPhoneNumber, Integer newId) {
 		super(newName, newCellPhoneNumber, newId);
-		// TODO Auto-generated constructor stub
-	}
-
-	public CustomerSQL(String fullyQualifiedExportedCustomerString)
-			throws Exception {
-		super(fullyQualifiedExportedCustomerString);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @param args
+	 * Creates a new Customer based on the response of an SQL query with one user
+	 * @param fullyQualifiedSQLReply
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public CustomerSQL(String fullyQualifiedSQLReplyString)
+			throws Exception {
+		//1. create the new customer based on super method
+		super(fullyQualifiedSQLReplyString);
+		// TODO Auto-generated constructor stub
 	}
+		/**
+	 * Function which export an existing customer object as a VALUES part for an SQL statement
+	 * 
+	 * @return VALUES(ID,Owner,Rate,Name,CellPhoneNumber,LandlinePhoneNumber,AirtimeMinutes) 
+	 *         
+	 * @author benste
+	 */
+	@Override
+	public String exportText(String owner) {
+		/*
+		 * The following section does create an array with the args of the class
+		 */
+		String[] parts = new String[Customer.IMPLEMENTEDARGS+1]; // parent
+		parts[0] = Integer.toString(this.getId())+",";
+		parts[1] = "\'"+owner+"\'"+",";
+		parts[2] = Integer.toString(this.getRate())+",";
+		parts[3] = this.getName()+",";
+		parts[4] = "\'"+this.getCellPhoneNumber()+"\'"+",";
+		parts[5] = "\'"+this.getLandlinePhoneNumer()+"\'"+",";
+		parts[6] = Integer.toString(this.getAirtimeMinutes())+",";
+		parts[7] = this.getBalance().toString();
+		
+		// Parse the Array into a String
+		String result = "VALUES(";
+		for (String part : parts) {
+			result += part + ",";
+		} // end for all items
+		result += ")";
+		return result;
+	} // end exportText()
+	
+
 	@Override
 	public ArrayList<CustomerCall> getCalls() {
 	}
